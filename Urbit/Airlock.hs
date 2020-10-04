bae7e7e2f6e8134731915b92d1ad36fdb68504f8
Ben Sima <ben@bsima.me>
2020-10-04 09:27:49 -0400

    formatting with ormolu

--- Urbit/Airlock.hs
+++ Urbit/Airlock.hs
@@ -2,22 +2,23 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Urbit.Airlock
-  ( Ship(..),
+  ( Ship (..),
     App,
     Mark,
     connect,
     poke,
-  ) where
+  )
+where
 
 import Control.Lens
 import qualified Data.Aeson as Aeson
 import Data.ByteString.Lazy (ByteString)
 import Data.Text (Text)
-import Network.Wreq (FormParam((:=)))
+import Network.Wreq (FormParam ((:=)))
 import qualified Network.Wreq as Wreq
 import qualified Network.Wreq.Session as Session
--- import qualified Network.Wai.EventSource as Event
 
+-- import qualified Network.Wai.EventSource as Event
 
 data Ship = Ship
   { session :: Maybe Session.Session,
@@ -28,28 +29,29 @@ data Ship = Ship
     -- | Login code, `+code` in the dojo. Don't share this publically.
     code :: Text,
     -- | Not implemented yet...
-    sseClient :: Bool,
-    -- | The `@p` of the ship
-    name :: Text
+    sseClient :: Bool
   }
   deriving (Show)
 
-
 channelUrl :: Ship -> String
-channelUrl Ship { url } = url <> "/channel.js"
-
+channelUrl Ship {url} = url <> "/channel.js"
 
 type Url = String
+
 type App = Text
+
 type Path = Text
+
 type Mark = Text
+
 type Subscription = Text
 
+-- | The `@p` for the ship.
+type ShipName = Text
 
 -- |
 nextEventId :: Ship -> Int
-nextEventId Ship { lastEventId } = lastEventId + 1
-
+nextEventId Ship {lastEventId} = lastEventId + 1
 
 -- | Connect and login to the ship.
 connect :: Ship -> IO (Wreq.Response ByteString)
@@ -59,51 +61,53 @@ connect ship = do
   r <- Wreq.getWith params (url ship <> "/~/login")
   return r
 
-
 -- | Poke a ship.
 poke ::
+  Aeson.ToJSON a =>
   Ship ->
+  -- | To what ship will you send the poke?
+  ShipName ->
   -- | Which gall application are you trying to poke?
   App ->
   -- | What mark should be applied to the data you are sending?
   Mark ->
-  Aeson.Value ->
+  a ->
   IO (Wreq.Response ByteString)
-poke ship app mark json = do
-  r <- Wreq.put (channelUrl ship)
-         ["id" := nextEventId ship
-         , "action" := ("poke" :: Text)
-         , "ship" := (name ship)
-         , "app" := app
-         , "mark" := mark
-         , "json" := Aeson.encode json
-         ]
+poke ship shipName app mark json = do
+  r <-
+    Wreq.put
+      (channelUrl ship)
+      [ "id" := nextEventId ship,
+        "action" := ("poke" :: Text),
+        "ship" := shipName,
+        "app" := app,
+        "mark" := mark,
+        "json" := Aeson.encode json
+      ]
   return r
-
 
 -- | Acknowledge receipt of a message. (This clears it from the ship's queue.)
 ack :: Ship -> Int -> IO (Wreq.Response ByteString)
 ack ship eventId = do
-  r <- Wreq.post (channelUrl ship)
-        ["action" := ("ack"::Text)
-        , "event-id" := eventId]
+  r <-
+    Wreq.post
+      (channelUrl ship)
+      [ "action" := ("ack" :: Text),
+        "event-id" := eventId
+      ]
   return r
-
 
 -- TODO
 -- ssePipe :: Ship -> IO _
 -- ssePipe ship = undefined
 
-
 -- |
 subscribe :: Ship -> App -> Path -> IO Subscription
 subscribe = undefined
 
-
 -- |
 unsubscribe :: Ship -> Subscription -> IO ()
 unsubscribe = undefined
-
 
 -- |
 delete :: Ship -> IO ()
