5c723787a75bfbb0be49b62978d17bfeafec5945
Ben Sima <ben@bsima.me>
2020-10-04 09:26:43 -0400

    better nix shell

--- shell.nix
+++ shell.nix
@@ -1,2 +1,11 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+
+nixpkgs.mkShell {
+  name = "urbit-airlock-shell";
+  buildInputs = [
+    nixpkgs.ormolu.bin
+    (nixpkgs.pkgs.haskell.packages.${compiler}.ghcWithPackages (hp: with hp; [
+      aeson base bytestring lens text wai wai-extra wreq
+    ]))
+  ];
+}
