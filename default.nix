5189924e35e4152ae793ed2d05fd320259ea1bb3
Ben Sima <ben@bsima.me>
2020-11-13 15:41:31 -0500

    Pin nixpkgs

--- default.nix
+++ default.nix
@@ -1,2 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc884" }:
+{ nixpkgs ? import (builtins.fetchtarball (import ./nixpkgs.nix)) {}
+, compiler ? "ghc884"
+}:
 nixpkgs.pkgs.haskell.packages.${compiler}.callPackage ./urbit-airlock.nix { }
---
+++ nixpkgs.nix
@@ -0,0 +1,5 @@
+# generated with ~/bin/nixpkgs-pin
+{
+  url = "https://github.com/NixOS/nixpkgs/archive/13209156c191524437d5556bd8b24a132c4a899a.tar.gz";
+  sha256 = "06g21xf0nd0r2nziw72qk3vp8cd3vdy39snd44fir6j3fyjx1p98";
+}
